54ca38cedd47b825ccf2c8fb8fe054e3bb4abb7c
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-16 21:19:38 +0530

    risponsive done.

---
+++ script.js
@@ -0,0 +1,43 @@
+ $(function() {
+    var nav = $(".nav");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 100) {
+           nav.addClass("border-black");
+        } else {
+            nav.removeClass("border-black")
+        }
+    });
+  
+ $(function() {
+
+    $('.tab-panels .tabs li').on('click', function() {
+
+        var $panel = $(this).closest('.tab-panels');
+
+        $panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        //figure out which panel to show
+        var panelToShow = $(this).attr('rel');
+
+        //hide current panel
+        $panel.find('.panel.active').slideUp(300, showNextPanel);
+
+        //show next panel
+        function showNextPanel() {
+            $(this).removeClass('active');
+
+            $('#'+panelToShow).slideDown(300, function() {
+                $(this).addClass('active');
+            });
+        }
+    });
+
+
+});
+
+
+
+});
